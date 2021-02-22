e102413383d4cad35a44bf3c380a2ffe7ba033a6
MrUniverse44 <justinalxhd@gmail.com>
2021-02-22 16:09:58 -0500

    Initial commit

---
+++ CookieFriends-bungeecord/src/main/java/dev/mruniverse/cookiefriends/BungeeFriends.java
@@ -0,0 +1,11 @@
+package dev.mruniverse.cookiefriends;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeeFriends extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
---
+++ CookieFriends-spigot/src/main/java/dev/mruniverse/cookiefriends/SpigotFriends.java
@@ -0,0 +1,12 @@
+package dev.mruniverse.cookiefriends;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotFriends extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
